a48fa3edaf0c8ed1cb16af8046fed7be5bdc6854
YoussefMagedMohamed <joeyoussef307@gmail.com>
2024-02-29 20:23:33 +0200

    Barnd & Category Components

--- src/Components/Brands/Brands.jsx
+++ src/Components/Brands/Brands.jsx
@@ -1,7 +1,42 @@
+import axios from 'axios';
 import React from 'react'
+import { Bars } from 'react-loader-spinner';
+import { useQuery } from 'react-query';
 
 export default function Brands() {
+    // Create Function Get All Products
+    function getAllBrands() {
+      return axios.get(`https://ecommerce.routemisr.com/api/v1/brands`);
+    }
+  
+    // Use useQuery Hook To manage Data
+    let { data, isLoading } = useQuery("allbrands", getAllBrands);
   return <>
-    <h1>Brands</h1>
+    <>
+      {isLoading ? (
+        <div className="row vh-100">
+          <Bars
+            height="100"
+            width="100"
+            color="#4fa94d"
+            ariaLabel="bars-loading"
+            wrapperStyle={{}}
+            wrapperClass="d-flex justify-content-center mt-5"
+            visible={true}
+          />
+        </div>
+      ) : (
+        <div className="row gy-5">
+          {data.data.data.map((brand) => (
+            <div className="col-md-4 p-5" key={brand._id}>
+              <div className="product p-2">
+                <img src={brand.image} className="w-100" alt="" />
+                <h2 className="bg-main text-light p-3 text-center">{brand.name}</h2>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
+    </>
   </>
 }
--- src/Components/Categories/Categories.jsx
+++ src/Components/Categories/Categories.jsx
@@ -1,7 +1,43 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Bars } from "react-loader-spinner";
+import { useQuery } from "react-query";
 
 export default function Categories() {
-  return <>
-  <h1>Categories</h1>
-</>
+  // Create Function Get All Products
+  function getAllCategories() {
+    return axios.get(`https://ecommerce.routemisr.com/api/v1/categories`);
+  }
+
+  // Use useQuery Hook To manage Data
+  let { data, isLoading } = useQuery("allcategories", getAllCategories);
+
+  return (
+    <>
+      {isLoading ? (
+        <div className="row vh-100">
+          <Bars
+            height="100"
+            width="100"
+            color="#4fa94d"
+            ariaLabel="bars-loading"
+            wrapperStyle={{}}
+            wrapperClass="d-flex justify-content-center mt-5"
+            visible={true}
+          />
+        </div>
+      ) : (
+        <div className="row gy-5">
+          {data.data.data.map((category) => (
+            <div className="col-md-4 p-5" key={category._id}>
+              <div className="product p-2">
+                <img src={category.image} className="w-100" alt="" />
+                <h2 className="bg-main text-light p-3 text-center">{category.name}</h2>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
+    </>
+  );
 }
